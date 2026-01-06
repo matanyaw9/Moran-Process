1618881a3428f42b3248f2ac97aff071f77b6075
Matanya Wiener <matanya.wiener@weizmann.ac.il>
2026-01-06 17:52:21 +0200

    wip

--- step_temperature.py
+++
@@ -1,9 +0,0 @@
-from population_graph import PopulationGraph
-from process_run import ProcessRun
-
-N = 10
-sim = ProcessRun(PopulationGraph.complete_graph(N=N), selection_coefficient=1.1)
-sim.initialize_random_mutant(n_mutants=5)
-# history = sim.run(track_history=True)['history']
-# history
-            
