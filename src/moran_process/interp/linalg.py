1f5932ba31a5cb0109bfbaadb1ead4dd38e3ab16
Hila Friedman <red.black.liquorice@gmail.com>
2026-07-20 12:45:57 +0300

    use SciPy sparse arrays, write example notebook

--- src/moran_process/interp/linalg.py
+++ src/moran_process/interp/linalg.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import sparse
 import itertools
 from enum import Enum
 
@@ -11,73 +12,88 @@ class Genesis(Enum):
 
 
 class TransitionMatrix:
-    # TODO: change this to a SciPy sparse matrix
-    #
-    # transition matrix; m[i, j] represents
-    # probability to go from state i to state j.
-    #
-    # state i is to be understood as a boolean vector,
-    # where the active bits encode the vertices which
-    # contain a mutant. e.g. state 6 (0b0110) has
-    # mutants in vertices no. 1, 2.
-    mat: np.ndarray
+    # within the stored matrices, each state i is to be understood as a boolean
+    # vector, where the active bits encode the vertices which contain a mutant.
+    # e.g. state 6 (0b0110) has mutants in vertices no. 1, 2.
+
+    # transient-to-transient matrix. dimensions are (2ⁿ-2, 2ⁿ-2).
+    # q[i, j] represents the probability to go from state i-1 to state j-1.
+    q: sparse.csr_array
+    # transient-to-mutent-extinction matrix. dimensions are (2ⁿ-2, 1).
+    # r_none[i, 0] represents the probability to go from state i-1 to state 0.
+    r_none: sparse.csc_array
+    # transient-to-mutant-takeover matrix. dimenstions are (2ⁿ-2, 1).
+    # r_all[i, 0] represents the probability to go from state i-1 to state
+    # 2ⁿ-1.
+    r_all: sparse.csc_array
     graph: GraphCore
 
     def __init__(self, g: GraphCore, r: float):
         n = g.n_nodes
-        m = np.zeros((2**n, 2**n))
+        q = sparse.lil_array((2**n - 2, 2**n - 2))
+        r_none = sparse.lil_array((2**n - 2, 1))
+        r_all = sparse.lil_array((2**n - 2, 1))
 
-        for i in range(2**n):
+        for i in range(1, 2**n - 1):
             denum = (r - 1) * i.bit_count() + n
             for src in range(n):
                 src_mut = (i >> src) & 1 != 0
                 neighs = g.nbrs[g.offsets[src] : g.offsets[src + 1]]
+                src_denum = denum * len(neighs)
                 for dst in neighs:
                     j = i | (1 << dst) if src_mut else i & ~(1 << dst)
-                    m[i, j] += (r if src_mut else 1) / denum / len(neighs)
+                    if j == 0:
+                        r_none[i - 1, 0] += 1 / src_denum
+                    elif j == 2**n - 1:
+                        r_all[i - 1, 0] += r / src_denum
+                    else:
+                        q[i - 1, j - 1] += (r if src_mut else 1) / src_denum
 
-        self.mat = m
+        self.q = q.tocsr()
+        self.r_none = r_none.tocsc()
+        self.r_all = r_all.tocsc()
         self.graph = g
 
     def fixation_prob(self, gen: Genesis) -> np.float64:
-        q = self.mat[1:-1, 1:-1]
-        r = self.mat[1:-1, -1]
-        gv = self._genesis_vec(gen)[1:-1]
-        return gv @ np.linalg.solve(np.identity(q.shape[1]) - q, r)
+        q = self.q
+        r = self.r_all
+        gv = self._genesis_vec(gen)
+        iq = sparse.identity(q.shape[1]) - q
+        res = gv @ sparse.linalg.spsolve(iq, r)
+        return res[0]
 
     def fixation_time(self, gen: Genesis) -> np.float64:
-        q = self.mat[1:-1, 1:-1]
-        r = self.mat[1:-1, -1]
-        inv_row_sums = 1 / np.sum(self.mat[1:-1, 1:], axis=1)
-        # don't use `*=`, that'll mutate self.mat!
-        q = q * inv_row_sums.reshape((-1, 1))
-        r = r * inv_row_sums
-        gv = self._genesis_vec(gen)[1:-1]
-        return gv @ np.linalg.solve(
-            np.linalg.matrix_power(np.identity(q.shape[1]) - q, 2), r
-        )
+        # TODO: this toarray() call makes for a big
+        # allocation. see if this can be rewritten sparsely.
+        row_sums = 1 - self.r_none.toarray()
+        q = self.q / row_sums
+        r = self.r_all / row_sums
+        gv = self._genesis_vec(gen)
+        iq = sparse.identity(q.shape[1]) - q
+        res = gv @ sparse.linalg.spsolve(iq @ iq, r)
+        return res[0]
 
     def absorb_time(self, gen: Genesis) -> np.float64:
-        q = self.mat[1:-1, 1:-1]
-        r = self.mat[1:-1, 1] + self.mat[1:-1, -1]
-        gv = self._genesis_vec(gen)[1:-1]
-        return gv @ np.linalg.solve(
-            np.linalg.matrix_power(np.identity(q.shape[1]) - q, 2), r
-        )
+        q = self.q
+        r = self.r_none + self.r_all
+        gv = self._genesis_vec(gen)
+        iq = sparse.identity(q.shape[1]) - q
+        res = gv @ sparse.linalg.spsolve(iq @ iq, r)
+        return res[0]
 
-    def _genesis_vec(self, gen: Genesis) -> np.ndarray:
+    def _genesis_vec(self, gen: Genesis) -> sparse.csr_array:
         g = self.graph
-        v = np.zeros(2**g.n_nodes)
+        v = sparse.lil_array((1, 2**g.n_nodes - 2))
         node_prob = 1 / g.n_nodes
         match gen:
             case Genesis.spontaneous:
                 for src in range(g.n_nodes):
-                    v[1 << src] = node_prob
+                    v[0, (1 << src) - 1] = node_prob
             case Genesis.temperature:
                 for src in range(g.n_nodes):
                     neighs = g.nbrs[g.offsets[src], g.offsets[src + 1]]
                     for dst in neighs:
-                        v[1 << dst] += node_prob / len(neighs)
+                        v[0, (1 << dst) - 1] += node_prob / len(neighs)
             case _:
                 assert False
-        return v
+        return v.tocsr()
